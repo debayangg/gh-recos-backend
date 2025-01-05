3f63741d782ef9cc702611f28791753d23ba6b01
Debayan Ghosh <debayanghosh852@gmail.com>
2025-01-05 18:23:08 +0530

    Added CORS

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import time
 from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
@@ -135,6 +136,15 @@ class ReqParams(BaseModel):
     language: str
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 tags_by_domain = {
     "NLP": ["nlp", "text-analysis", "language-models", "transformers", "chatbot"],
